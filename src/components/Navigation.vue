ca971fcc77eb61475249ad6261f6a7c37aecf352
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-22 00:07:29 -0400

    added 'is-active' toggle for nav menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,32 +4,27 @@
     <a class="nav-item">
       <h1 class="title is-4">@vixgrit</h1>
     </a>
-  </div>
-
-  <div class="nav-right">
-    <a class="nav-item">
+    <a class="nav-item" href="https://github.com/vixgrit">
       <span class="icon">
         <i class="fa fa-github"></i>
       </span>
     </a>
-    <a class="nav-item">
+    <a class="nav-item" href="https://twitter.com/vixgrit">
       <span class="icon">
         <i class="fa fa-twitter"></i>
       </span>
     </a>
   </div>
-
   <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
   <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-  <span class="nav-toggle">
+  <span class="nav-toggle" :class="{'is-active': isActive}" @click="toggleIsActive">
     <span></span>
     <span></span>
     <span></span>
   </span>
-
   <!-- This "nav-menu" is hidden on mobile -->
   <!-- Add the modifier "is-active" to display it on mobile -->
-  <div class="nav-right nav-menu">
+  <div class="nav-right nav-menu" :class="{'is-active': isActive}">
     <a class="nav-item">
       Home
     </a>
@@ -51,6 +46,16 @@
 
 <script>
 export default {
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleIsActive() {
+      this.isActive = !this.isActive;
+    }
+  }
 }
 </script>
 
